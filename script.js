ee891dcc18439049bb87e86100fa1572c1334c21
Festus Omole <festusomole14@gmail.com>
2020-04-10 21:27:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const periodType = document.querySelectorAll("[data-period-type]");
+const timeToElapse = document.querySelector("[data-time-to-elapse]");
+const reportedCases = document.querySelector("[data-reported-cases]");
+const population = document.querySelector("[data-population]");
+const totalHospitalBeds = document.querySelector("[data-total-hospital-beds]");
+const submitButton = document.querySelector("[data-go-estimate]");
+
+const getRadioValue = () => {
+  for (let elem in periodType) {
+    if (periodType[elem].checked) return periodType[elem].value;
+  }
+};
+
+const getInputData = () => {
+  const data = {};
+  data.populationValue = population.value;
+  data.timeToElapseValue = timeToElapse.value;
+  data.reportedCasesValue = reportedCases.value;
+  data.totalHospitalBedsValue = totalHospitalBeds.value;
+  data.periodTypeValue = getRadioValue();
+  return data;
+};
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const data = getInputData();
+  console.log(data);
+});
