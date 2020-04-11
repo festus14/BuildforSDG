ffbb3b87ba916e474f7d345e329eaa17c55d0c86
Festus Omole <festusomole14@gmail.com>
2020-04-11 14:48:04 +0100

    Changed radio to select

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-const getRadioValue = () => {
-  for (let elem in periodTypes) {
-    if (periodTypes[elem].checked) return periodTypes[elem].value;
-  }
-};
-
 const getInputData = () => {
   const data = {};
   data.populationValue = population.value;
   data.timeToElapseValue = timeToElapse.value;
   data.reportedCasesValue = reportedCases.value;
   data.totalHospitalBedsValue = totalHospitalBeds.value;
-  data.periodTypeValue = getRadioValue();
+  data.periodTypeValue = periodTypes.value;
   return data;
 };
