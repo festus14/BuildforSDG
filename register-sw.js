1fcbf3731d3905c9203ad2a809dfd61ec359e776
Festus Omole <festusomole14@gmail.com>
2020-04-11 12:06:22 +0100

    Removed console and added label

--- register-sw.js
+++ register-sw.js
@@ -1,17 +1,8 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker.register("/service-worker.js").then(
-      function (registration) {
-        // Registration was successful
-        // console.log(
-        //   "ServiceWorker registration successful with scope: ",
-        //   registration.scope
-        // );
-      },
-      function (err) {
-        // registration failed :(
-        // console.log("ServiceWorker registration failed: ", err);
-      }
+      function (registration) {},
+      function (err) {}
     );
   });
 }
--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,6 @@ self.addEventListener("install", function (event) {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      // console.log("Opened cache");
       return cache.addAll(urlsToCache);
     })
   );
