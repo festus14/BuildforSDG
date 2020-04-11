7280ec01b5e4a1a6a06c30b3bc7d96056af3bcd5
Festus Omole <festusomole14@gmail.com>
2020-04-11 13:34:26 +0100

    moved getting to index file (internal js)

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-const periodType = document.querySelectorAll("[data-period-type]");
-const timeToElapse = document.querySelector("[data-time-to-elapse]");
-const reportedCases = document.querySelector("[data-reported-cases]");
-const population = document.querySelector("[data-population]");
-const totalHospitalBeds = document.querySelector("[data-total-hospital-beds]");
-const submitButton = document.querySelector("[data-go-estimate]");
-
 const getRadioValue = () => {
   for (let elem in periodType) {
     if (periodType[elem].checked) return periodType[elem].value;
